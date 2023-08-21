2794ca05156acced975287f3215fdde4c06af799
Tristan Vidal <trsgot@gmail.com>
2023-08-21 18:04:11 -0500

    Move to Astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    colors: {
+      white: '#eeeffc',
+      gray: '#81859f',
+      black: '#000',
+      blue: '#007CFB',
+      'light-gray': '#1e1e1e',
+      pink: '#8242cd',
+      red: '#ff0080'
+    }
+  },
+  plugins: []
+}
